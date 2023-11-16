e6a4a436f679db0422693440a392006d1529ff9e
TeooL <144486916+TeooL@users.noreply.github.com>
2023-11-15 21:47:44 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String [] args){
-        Algorithms a = new Algorithms();
-        System.out.println(a.numDigits(245));
+            Algorithm a = new Algorithm();
+            System.out.println("Print");
+            System.out.println(a.numDigits(245));
+            System.out.println(a.firstDivisibleSec(4,2));
+            for (int n = 1; n < 1; n++)System.out.println("this is printed once.");
     }
 }
