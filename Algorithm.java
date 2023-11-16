b21898b6aa82e0a7ef0bdf1cea5dc8f6d2b9934b
TeooL <144486916+TeooL@users.noreply.github.com>
2023-11-16 15:36:16 -0500

    Add files via upload

--- Algorithm.java
+++ Algorithm.java
@@ -1,15 +1,78 @@
 public class Algorithm {
-    public int numDigits(int number) {
-        int digits = 0;
-        while (number % 10 != 0) {
-            number /= 10;
-            digits++;
+  
+  public int numDigits(int number) {
+    int digits = 0;
+    while (number % 10 != 0) {
+      number /= 10;
+      digits++;
+    }
+    return digits;
+  }
+  
+  public boolean firstDivisibleSec(int first, int second) {
+    if (((double) first / (double) second) == first / second) return true;
+    return false;
+  }
+  
+  public int sumOfOdd(int first, int second){
+    int sum = 0;
+    int start = 0;
+    if (first > second) {
+      start = second;
+      while (start <= first){
+        if (start % 2 != 0){
+          sum += start;
         }
-        return digits;
+        start ++;
+      }
     }
-
-    public boolean firstDivisibleSec(int first, int second) {
-        if (((double) first / (double) second) == first / second) return true;
-        return false;
+    else {
+      start = first;
+      while (start <= second){
+        if (start % 2 != 0){
+          sum += start;
+        }
+        start++;
+      }
     }
+    return sum;
+  }
+  
+  public int sumOfTens(int first, int second){
+    int sum = 0;
+    int start = 0;
+    if (first > second){
+      start = second;
+      while (start <= first){
+        if (start % 10 == 0)sum += start;
+        start ++;
+      }
+    }
+    else {
+      start = first;
+      while (start <= second){
+        if (start % 10 == 0)sum += start;
+        start ++;
+      }
+    }
+    return sum;
+  }
+  
+  public int sumOfDigits(int number){
+    int sum = 0;
+    int digits = numDigits(number);
+    int count = 0;
+    int tenths_place = digits;
+    while (count != digits){
+      sum += num % (1 * Math.abs(10,tenths_place));
+      count ++;              
+    return sum;
+    }
+  }
+  
+  public int largestPowOfTwo(int number){
+    int largest = 0;
+    return largest;
+  }
 }
+
--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String [] args){
             Algorithm a = new Algorithm();
-            System.out.println("Print");
-            System.out.println(a.numDigits(245));
-            System.out.println(a.firstDivisibleSec(4,2));
+            System.out.println(a.numDigits(24554));
+            System.out.println(a.firstDivisibleSec(5,2));
+            System.out.println(a.sumOfOdd(3,9));
+            System.out.println(a.sumOfTens(10,99));
             for (int n = 1; n < 1; n++)System.out.println("this is printed once.");
     }
 }
