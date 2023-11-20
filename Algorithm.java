cc6906a0d36a0b465e9e3c531c4a1c4eb0cde95b
TeooL <144486916+TeooL@users.noreply.github.com>
2023-11-19 20:34:19 -0500

    Add files via upload

--- Algorithm.java
+++ Algorithm.java
@@ -1,78 +1,82 @@
 public class Algorithm {
-  
-  public int numDigits(int number) {
-    int digits = 0;
-    while (number % 10 != 0) {
-      number /= 10;
-      digits++;
-    }
-    return digits;
-  }
-  
-  public boolean firstDivisibleSec(int first, int second) {
-    if (((double) first / (double) second) == first / second) return true;
-    return false;
-  }
-  
-  public int sumOfOdd(int first, int second){
-    int sum = 0;
-    int start = 0;
-    if (first > second) {
-      start = second;
-      while (start <= first){
-        if (start % 2 != 0){
-          sum += start;
+
+    public int numDigits(int number) {
+        int digits = 0;
+        while (number % 10 != 0) {
+            number /= 10;
+            digits++;
         }
-        start ++;
-      }
+        return digits;
     }
-    else {
-      start = first;
-      while (start <= second){
-        if (start % 2 != 0){
-          sum += start;
+
+    public boolean firstDivisibleSec(int first, int second) {
+        return ((double) first / second) == first / second;
+    }
+
+    public int sumOfOdd(int first, int second){
+        int sum = 0;
+        int start = 0;
+        if (first > second) {
+            start = second;
+            while (start <= first){
+                if (start % 2 != 0){
+                    sum += start;
+                }
+                start ++;
+            }
         }
-        start++;
-      }
+        else {
+            start = first;
+            while (start <= second){
+                if (start % 2 != 0){
+                    sum += start;
+                }
+                start++;
+            }
+        }
+        return sum;
     }
-    return sum;
-  }
-  
-  public int sumOfTens(int first, int second){
-    int sum = 0;
-    int start = 0;
-    if (first > second){
-      start = second;
-      while (start <= first){
-        if (start % 10 == 0)sum += start;
-        start ++;
-      }
+
+    public int sumOfTens(int first, int second){
+        int sum = 0;
+        int start = 0;
+        if (first > second){
+            start = second;
+            while (start <= first){
+                if (start % 10 == 0)sum += start;
+                start ++;
+            }
+        }
+        else {
+            start = first;
+            while (start <= second){
+                if (start % 10 == 0)sum += start;
+                start ++;
+            }
+        }
+        return sum;
     }
-    else {
-      start = first;
-      while (start <= second){
-        if (start % 10 == 0)sum += start;
-        start ++;
-      }
+
+    public int sumOfDigits(int number){
+        int sum = 0;
+        while (number > 0){
+            sum += number % 10;
+            number = number / 10;
+        }
+        return sum;
     }
-    return sum;
-  }
-  
-  public int sumOfDigits(int number){
-    int sum = 0;
-    int digits = numDigits(number);
-    int count = 0;
-    int tenths_place = digits;
-    while (count != digits){
-      sum += num % (1 * Math.abs(10,tenths_place));
-      count ++;              
-    return sum;
+
+    public int largestPowOfTwo(int number){
+        int power = 1;
+        while (true) {
+            if (Math.pow(2, power) <= number) {
+                power++;
+            }
+            else if (Math.pow(2,power) > number){
+                break;
+            }
+        }
+            return (int)(Math.pow(2,power-1));
     }
-  }
-  
-  public int largestPowOfTwo(int number){
-    int largest = 0;
-    return largest;
-  }
 }
 
--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String [] args){
-            Algorithm a = new Algorithm();
-            System.out.println(a.numDigits(24554));
-            System.out.println(a.firstDivisibleSec(5,2));
-            System.out.println(a.sumOfOdd(3,9));
-            System.out.println(a.sumOfTens(10,99));
-            for (int n = 1; n < 1; n++)System.out.println("this is printed once.");
+        Algorithm a = new Algorithm();
+        System.out.println(a.numDigits(987654321)); // Should Print 9
+        System.out.println(a.numDigits(4321)); // Should Print 4
+        System.out.println(a.numDigits(123456)); // Should Print 6
+        System.out.println(a.firstDivisibleSec(9,3)); // Should Print true
+        System.out.println(a.firstDivisibleSec(4,3)); // Should Print false
+        System.out.println(a.firstDivisibleSec(16,4)); // Should Print true
+        System.out.println(a.sumOfOdd(4,9)); // Should print 21
+        System.out.println(a.sumOfOdd(3,11)); // Should print 35
+        System.out.println(a.sumOfOdd(6,12)); // Should print 27
+        System.out.println(a.sumOfTens(10,99)); // Should print 450
+        System.out.println(a.sumOfTens(22,100)); // Should print 520
+        System.out.println(a.sumOfTens(29,117)); // Should print 630
+        System.out.println(a.sumOfDigits(321)); // Should print 6
+        System.out.println(a.sumOfDigits(47216)); // Should print 20
+        System.out.println(a.sumOfDigits(4511)); // Should print 11
+        System.out.println(a.largestPowOfTwo(33)); // Should print 32
+        System.out.println(a.largestPowOfTwo(16)); // Should print 16
+        System.out.println(a.largestPowOfTwo(800)); // Should print 512
     }
 }
