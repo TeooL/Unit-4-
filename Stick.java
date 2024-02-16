35b575ac2a314e800b5944e86dcf66f29169c258
TeooL <144486916+TeooL@users.noreply.github.com>
2024-02-16 15:21:12 -0500

    Add files via upload

---
+++ Stick.java
@@ -0,0 +1,9 @@
+public class Stick {
+    private int length;
+    public Stick(int n){
+        length = n;
+    }
+    public String toString(){
+        return "This stick is " + length + " millimeters long.";
+    }
+}
