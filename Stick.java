e1f9caaa73435f6b8c2c430bff4f65ff9981fe33
Teo <144486916+TeooL@users.noreply.github.com>
2024-10-14 16:45:42 -0400

    Delete Stick.java

--- Stick.java
+++
@@ -1,9 +0,0 @@
-public class Stick {
-    private int length;
-    public Stick(int n){
-        length = n;
-    }
-    public String toString(){
-        return "This stick is " + length + " millimeters long.";
-    }
-}
